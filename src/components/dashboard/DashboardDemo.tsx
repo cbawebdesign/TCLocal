18836ed52aae56554767b88fca3233ce388c7e7c
cbawebdesign <35353826+cbawebdesign@users.noreply.github.com>
2024-10-30 15:15:47 -0700

    comit

--- src/components/dashboard/DashboardDemo.tsx
+++ src/components/dashboard/DashboardDemo.tsx
@@ -20,7 +20,7 @@ export default function DashboardDemo() {
   return (
     <div className="flex flex-col items-center min-h-screen p-6 space-y-6">
       <LogoImage style={{ width: '160px', height: '100px' }} />
-      <h1 className="text-2xl font-bold mb-4">Welcome to your Trade Companion Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome to  Trade Companion Dashboard</h1>
       <img src="https://firebasestorage.googleapis.com/v0/b/test7-8a527.appspot.com/o/loader3.gif?alt=media&token=b095ed40-ac2d-4368-8bd3-e6f5bce9aa3e" alt="Loading..." className="mb-4" />
       <p className="text-center max-w-prose text-blue-700 italic">
         <ul className="list-disc list-inside">
